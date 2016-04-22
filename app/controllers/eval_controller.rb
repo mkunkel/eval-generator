ceb7804ae27a98909d78b733549fdf33b00d974c
Mike Kunkel <mike@mkunkel.com>
2016-04-22 13:20:54 -0500

    Initial commit
    Includes a form to enter crew and game information.

---
+++ app/controllers/eval_controller.rb
@@ -0,0 +1,27 @@
+class EvalController < ApplicationController
+  def show
+    render :show, locals: { referees: referees }
+  end
+
+  def create
+    require 'pry'
+    binding.pry
+  end
+
+  private
+
+  def referees
+    ['Head Referee', 'Inside Pack Referee', 'Jammer Referee', 'Jammer Referree',
+     'Outside Pack Referee', 'Outside Pack Referee', 'Outside Pack Referee'].map do |position|
+      {
+        position: position,
+        abbreviation: position_to_abbreviation(position)
+      }
+    end
+  end
+
+  def position_to_abbreviation(position)
+    words = position.split(' ')
+    words.map(&:first).join('').upcase
+  end
+end
